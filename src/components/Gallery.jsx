b283d409db07b6cb90195aa4f51950a9b4260685
Youssef97dev <youssefmoussafi97@gmail.com>
2024-10-21 13:17:20 +0100

    second comit

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const imagesUrl = [
   "/images/gallery/menu-gallery-4.jpg",
-  "/images/gallery/menu-hero.jpg",
+  "/images/gallery/menu-gallery-25.jpg",
   "/images/gallery/menu-gallery-16.jpg",
 ];
 
@@ -12,31 +13,34 @@ const Gallery = () => {
     <>
       <div className="hidden md:flex w-full justify-between items-center space-x-2 py-2">
         {imagesUrl.map((image, index) => (
-          <div key={index} className="relative">
-            <Image
-              src={image}
-              alt="le salama gallery"
-              width={600}
-              height={600}
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute inset-0 bg-[#000] h-full mix-blend-multiply opacity-35"></div>
-          </div>
+          <Link href="/gallery" target="_blank" className="cursor-pointer">
+            <div key={index} className="relative">
+              <Image
+                src={image}
+                alt="le salama gallery"
+                width={600}
+                height={600}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </Link>
         ))}
       </div>
-      <div className="flex h-full w-full justify-center items-center  relative md:hidden">
-        <Image
-          src="/images/gallery/menu-gallery-4.jpg"
-          alt="le salama gallery"
-          width={600}
-          height={700}
-          className="w-full h-full object-cover"
-        />
-        <div className="z-10 absolute inset-0 bg-[#000] h-full mix-blend-multiply opacity-35"></div>
-        <span className="absolute w-full h-full flex justify-center items-center text-4xl text-white z-20">
-          GALLERY
-        </span>
-      </div>
+      <Link href="/gallery" className="cursor-pointer">
+        <div className="flex h-full w-full justify-center items-center  relative md:hidden">
+          <Image
+            src="/images/gallery/menu-gallery-25.jpg"
+            alt="le salama gallery"
+            width={600}
+            height={700}
+            className="w-full h-full object-cover"
+          />
+          <div className="z-10 absolute inset-0 bg-[#000] h-full mix-blend-multiply opacity-35"></div>
+          <span className="absolute w-full h-full flex justify-center items-center text-4xl text-white z-20">
+            GALLERY
+          </span>
+        </div>
+      </Link>
     </>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,13 +13,13 @@ const Hero = () => {
     <div id="hero" className="relative h-screen w-full ">
       {/* Image Background */}
       <Image
-        src="/images/hero-1.jpg"
+        src="/images/hero-5.jpg"
         alt="le salama main"
         width={1000}
         height={1000}
         className="absolute top-0 left-0 w-full h-full object-cover"
       />
-      <div className="absolute inset-0 bg-[#000] h-screen mix-blend-multiply opacity-50"></div>
+      <div className="absolute inset-0 bg-[#000] h-screen mix-blend-multiply opacity-20"></div>
       {/* Hero Content */}
 
       <div className="absolute h-full w-full flex flex-col justify-center items-center text-white space-y-3">
