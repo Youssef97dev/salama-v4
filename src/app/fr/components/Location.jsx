f43d458d016f75c0dff203084bed5f0c2f8abbf5
Youssef97dev <youssefmoussafi97@gmail.com>
2024-10-21 15:33:25 +0100

    change location content

--- src/app/fr/components/Location.jsx
+++ src/app/fr/components/Location.jsx
@@ -8,18 +8,23 @@ const Location = () => {
       className="w-full h-full md:h-screen flex flex-col md:flex-row justify-center items-center"
     >
       <div className="h-full w-full md:w-1/2 flex flex-col justify-center items-center md:items-start text-primary px-10 py-10 md:py-0 space-y-3">
-        <span className="font-semibold text-lg md:text-2xl tracking-widest">
+        <span className="font-semibold text-lg md:text-2xl tracking-widest mb-2">
           LOCATION
         </span>
-        <p className="text-base md:text-xl text-center md:text-start">
-          {`Situé au cœur de la place Djema el Fna, c'est un endroit confortable pour prendre un verre après une séance de shopping dans les souks ! En cuisine, vous trouverez les incontournables de la cuisine marocaine : épaule d'agneau, Tangia, Kemias... le tout cuisiné avec subtilité. Côté ambiance, il y a un DJ et des danseurs, selon les soirs. Et ce n'est pas tout ! Venez explorer les secrets que cache ce lieu fascinant.`}
-        </p>
+        <span className="uppercase font-semibold tracking-widest">Adresse</span>
+        <span className="text-center">
+          40 Rue des Banques, Marrakech 40000, Maroc
+        </span>
+        <span className="uppercase font-semibold tracking-widest">
+          {`Horaires d'ouverture`}
+        </span>
+        <span className="text-center">Tous les jours : de 12h00 à 02h00</span>
         <Link
           href="https://www.google.com/maps/place/Le+Salama/@31.6264969,-7.9874887,173m/data=!3m1!1e3!4m6!3m5!1s0xdafee422b547dcb:0x77d12fb6b8ea2d42!8m2!3d31.6264238!4d-7.98697!16s%2Fg%2F1tcxr074?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D"
           target="_blank"
           className="uppercase border border-primary text-primary px-4 py-2 hover:bg-primary hover:text-primary_2 transition-all ease-linear"
         >
-          Ouvrir la MAP
+          Ouvrir MAP
         </Link>
       </div>
       <div className="bg-primary_5 h-full w-full md:w-1/2">
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -7,19 +7,18 @@ const Location = () => {
       id="location"
       className="w-full h-full md:h-screen flex flex-col md:flex-row justify-center items-center"
     >
-      <div className="h-full w-full md:w-1/2 flex flex-col justify-center items-center md:items-start text-primary px-10 py-10 md:py-0 space-y-3">
-        <span className="font-semibold text-lg md:text-2xl tracking-widest">
+      <div className="h-full w-full md:w-1/2 flex flex-col justify-center items-center md:items-start text-primary px-10 py-10 md:py-0 space-y-1">
+        <span className="font-semibold text-lg md:text-2xl tracking-widest mb-2">
           LOCATION
         </span>
-        <p className="text-base md:text-xl text-center md:text-start">
-          {`Located in the heart of Djema el Fna square, it’s a cozy place to
-          enjoy a drink after a shopping spree in the souks! In the kitchen,
-          you’ll find the must-try dishes of Moroccan cuisine: shoulder of lamb,
-          Tangia, Kemias... all cooked with subtlety. As for the ambiance,
-          there’s a pianist, DJ, or dancers, depending on the evening. And
-          there’s more to discover! Come and explore the secrets this
-          fascinating place hides.`}
-        </p>
+        <span className="uppercase font-semibold tracking-widest">Address</span>
+        <span className="text-center">
+          40 Rue des Banques, Marrakech 40000, Maroc
+        </span>
+        <span className="uppercase font-semibold tracking-widest">
+          Opening Hours
+        </span>
+        <span className="text-center">Daily : 12.00 PM to 02.00 AM</span>
         <Link
           href="https://www.google.com/maps/place/Le+Salama/@31.6264969,-7.9874887,173m/data=!3m1!1e3!4m6!3m5!1s0xdafee422b547dcb:0x77d12fb6b8ea2d42!8m2!3d31.6264238!4d-7.98697!16s%2Fg%2F1tcxr074?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D"
           target="_blank"
